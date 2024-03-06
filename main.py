44e4ec38bda6ca4192a214b411e38c68afb32830
Scoob <thatguyscoob1@gmail.com>
2024-03-06 13:28:32 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def on_button_pressed_a():
         basic.show_number(currentNum)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_gesture_logo_up():
+    global currentNum
+    if currentNum > 10:
+        currentNum = currentNum - 10
+        basic.show_number(currentNum)
+        basic.pause(5000)
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
 def on_button_pressed_ab():
-    global currentNum, streakNum
-    if currentNum != selectedNum:
-        currentNum = selectedNum
+    global selectedNum, streakNum, attemptNum
+    if selectedNum != currentNum:
+        selectedNum = currentNum
     if selectedNum == answerNum:
         basic.show_leds("""
             . . . . .
@@ -18,6 +26,7 @@ def on_button_pressed_ab():
             . . # . .
             """)
         streakNum = streakNum + 1
+        basic.show_string("Attempts:" + ("" + str(attemptNum)))
     elif selectedNum != answerNum:
         basic.show_leds("""
             # . . . #
@@ -27,6 +36,11 @@ def on_button_pressed_ab():
             # . . . #
             """)
         streakNum = 0
+        attemptNum = attemptNum + 1
+        if selectedNum < answerNum:
+            basic.show_arrow(ArrowNames.NORTH)
+        elif selectedNum > answerNum:
+            basic.show_arrow(ArrowNames.SOUTH)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
@@ -37,19 +51,28 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
-    control.reset()
+    global answerNum, selectedNum, currentNum, attemptNum
+    answerNum = randint(1, 100)
+    selectedNum = 0
+    currentNum = 0
+    attemptNum = 0
+    basic.pause(5000)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-streakNum = 0
-answerNum = 0
-currentNum = 0
-selectedNum = 0
-selectedNum = 0
-currentNum = 0
-answerNum = randint(1, 100)
+def on_gesture_logo_down():
+    global currentNum
+    if currentNum <= 90:
+        currentNum = currentNum + 10
+        basic.show_number(currentNum)
+        basic.pause(5000)
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
 
-def on_forever():
-    (answerNum)
-    (currentNum)
-    (selectedNum)
-basic.forever(on_forever)
+attemptNum = 0
+streakNum = 0
+currentNum = 0
+selectedNum = 0
+answerNum = 0
+answerNum = randint(1, 100)
+selectedNum = 0
+currentNum = 0
+serial.write_line("Answer:" + ("" + str(answerNum)))
--- main.ts
+++ main.ts
@@ -1,20 +1,26 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (currentNum < 100) {
         currentNum = currentNum + 1
         basic.showNumber(currentNum)
     }
+    
 })
-input.onGesture(Gesture.LogoUp, function () {
+input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
+    
     if (currentNum > 10) {
         currentNum = currentNum - 10
         basic.showNumber(currentNum)
         basic.pause(5000)
     }
+    
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     if (selectedNum != currentNum) {
         selectedNum = currentNum
     }
+    
     if (selectedNum == answerNum) {
         basic.showLeds(`
             . . . . .
@@ -24,7 +30,7 @@ input.onButtonPressed(Button.AB, function () {
             . . # . .
             `)
         streakNum = streakNum + 1
-        basic.showString("Attempts:" + attemptNum)
+        basic.showString("Attempts:" + ("" + ("" + attemptNum)))
     } else if (selectedNum != answerNum) {
         basic.showLeds(`
             # . . . #
@@ -40,27 +46,34 @@ input.onButtonPressed(Button.AB, function () {
         } else if (selectedNum > answerNum) {
             basic.showArrow(ArrowNames.South)
         }
+        
     }
+    
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (currentNum > 1) {
         currentNum = currentNum - 1
         basic.showNumber(currentNum)
     }
+    
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     answerNum = randint(1, 100)
     selectedNum = 0
     currentNum = 0
     attemptNum = 0
     basic.pause(5000)
 })
-input.onGesture(Gesture.LogoDown, function () {
+input.onGesture(Gesture.LogoDown, function on_gesture_logo_down() {
+    
     if (currentNum <= 90) {
         currentNum = currentNum + 10
         basic.showNumber(currentNum)
         basic.pause(5000)
     }
+    
 })
 let attemptNum = 0
 let streakNum = 0
@@ -70,4 +83,4 @@ let answerNum = 0
 answerNum = randint(1, 100)
 selectedNum = 0
 currentNum = 0
-serial.writeLine("Answer:" + answerNum)
+serial.writeLine("Answer:" + ("" + ("" + answerNum)))
